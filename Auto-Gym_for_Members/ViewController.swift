df10c7f7dbfa0d10e32c90a0c0793eb3a5715b09
Marcio Rosemberg <marcio@dsdds-pc.sysnet.local>
2018-12-11 10:57:07 -0800

    Initial Commit

---
+++ Auto-Gym_for_Members/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto-Gym_for_Members
+//
+//  Created by Marcio R. Rosemberg on 11/12/18.
+//  Copyright © 2018 Marcio R. Rosemberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
